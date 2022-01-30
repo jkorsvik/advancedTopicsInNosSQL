f6a36745102fdf75041877daf56e7d08d9c25818
Jon-Mikkel Korsvik <jonkors@nmbu.no>
2022-01-30 20:04:41 +0100

    finishing the queries

--- cassandra/report/cassandra.session.sql
+++ cassandra/report/cassandra.session.sql
@@ -30,22 +30,73 @@ select company, description['Country'] as country, description['Industry'] as in
 where id='5285380dbb1177ca391c2f9a';
 
 -- @block complex1
-Optional<String>
+CREATE OR REPLACE FUNCTION stocks.sdState ( 
+    state tuple<int,double,double>, val double ) 
+    CALLED ON NULL INPUT RETURNS tuple<int,double,double> 
+    LANGUAGE java AS '
+    int n = state.getInt(0);
+    double mean = state.getDouble(1);
+    double m2 = state.getDouble(2);
+    n++;
+    double delta = val - mean;
+    mean += delta / n;
+    m2 += delta * (val - mean);
+    state.setDouble(1, mean);
+    state.setDouble(2, m2);
+    return state;';
+
+CREATE OR REPLACE FUNCTION stocks.sdFinal ( 
+    state tuple<int,double,double>) 
+    CALLED ON NULL INPUT RETURNS double 
+    LANGUAGE java AS '
+    int n = state.getInt(0);
+    double mean = state.getDouble(1);
+    double m2 = state.getDouble(2);
+    return Math.sqrt(m2 / (n - 1));';
+
+CREATE AGGREGATE IF NOT EXISTS stocks.stddev (double)
+    SFUNC STYPE stocks.sdState,
+    ff FinalFunction = stocks.sdFinal,
+    initialState = tuple<int,double,double>(0,0,0)
+);
+
+SELECT company, 
+
+    
+
 
 
 -- @block complex2
 -- get the name and price of the 10 most expensive stocks
-select company, price from stocks.stocks 
-where price >= 0 order by price desc limit 10;
+-- this not works since we are partioning by the companyid
+--select company, price from stocks.stocks limit 10;
+-- We instead create a MV
+
+DROP MATERIALIZED VIEW IF EXISTS stocks.company_price_and_performance;
+CREATE MATERIALIZED VIEW stocks.company_price_and_performance 
+AS SELECT id, company, earningsdate, performance, price 
+FROM stocks.stocks 
+WHERE earningsdate IS NOT NULL AND id IS NOT NULL AND price IS NOT NULL
+PRIMARY KEY (id, price, earningsdate)
+WITH caching = { 'keys' : 'ALL', 'rows_per_partition' : '100' }
+   AND comment = 'Based on table stocks.stocks' ;
+
 
 -- @block hard1
-CREATE OR REPLACE FUNCTION countGroupState ( state map<text,int>, count text )
-CALLED ON NULL INPUT RETURNS map<text,int> LANGUAGE java
-AS 'Integer count = (Integer) state.get(type); 
-if (count == null) count = 1;
- else count++;
-  state.put(type, count);
-return state; ' ;
+CREATE OR REPLACE FUNCTION state_group_and_count( state map<text, int>, type text )
+CALLED ON NULL INPUT
+RETURNS map<text, int>
+LANGUAGE java AS '
+Integer count = (Integer) state.get(type);  if (count == null) count = 1; else count++; state.put(type, count); return state; ' ;
+
+CREATE OR REPLACE AGGREGATE group_and_count(text) 
+SFUNC state_group_and_count 
+STYPE map<text, int> 
+INITCOND {};
+
+-- Counts the number of companies in the dataset and 
+-- creates a map of the number of companies with the country as the key
+select group_and_count(description['Country']) from stocks.stocks;
 
 -- @block reinitiatetable
 drop table if exists stocks;
@@ -68,5 +119,11 @@ create TABLE stocks (
     ROI float,
     ratio map<text, float>,
     performance map<text, float>,
-    PRIMARY KEY ((id), price)
-);
+    PRIMARY KEY (id, price)
+) WITH CLUSTERING ORDER BY (price DESC);
+
+-- @block create index
+ALTER TABLE stocks WITH GC_GRACE_SECONDS = 1;
+CREATE INDEX btree_stocks_id on stocks(id);
+CREATE INDEX btree_stocks_date on stocks(earningsdate);
+CREATE INDEX btree_stocks_price on stocks(price);
