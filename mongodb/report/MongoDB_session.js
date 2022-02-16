626e189c3c368240d226a10fa160f0d918884377
Jon-Mikkel Korsvik <jonkors@nmbu.no>
2022-02-16 18:08:57 +0100

    added hard query

--- mongodb/report/MongoDB_session.js
+++ mongodb/report/MongoDB_session.js
@@ -68,3 +68,51 @@ db.stocks.aggregate(
 
 //Hard Query
 //--9--
+
+const match_eps =  {
+    $match: { EPSttm : { $gte : 0} }
+ };
+
+const grouping = {
+    $group: { _id:{
+      country: "$description.Country", 
+      sector: "$description.Sector" 
+      }, 
+      // Avg volume, price, and number of trading days in a year
+      // to approximate the traded value of the company in the span of ayear
+     sum_volume: { $sum: "$avg_vol" },
+     sum_price: { $sum: "$price" },
+         }
+ }
+const sorting = { 
+          $sort : { 
+              Valueoftrades : -1.0
+          
+  }};
+const projection = {$project : {
+              country : 1.0,
+              sector : 1.0,
+              Valueoftrades: {
+                $multiply: ["$sum_volume", "$sum_price", 253]
+                }
+              }
+  }
+// Build an aggregation to see what the values traded in a year
+// of the different sectors in countries are, for profitable business
+const aggregation = [
+
+   // Stage 1 filter for profitable businesses
+match_eps,
+ // stage 2 group to find the total amount of value 
+ // traded grouped by country and sector
+grouping,
+projection,
+sorting 
+];
+
+// Run the aggregation and open a cursor to the results.
+// Use toArray() to exhaust the cursor to return the whole result set.
+// You can use hasNext()/next() to iterate through the cursor page by page.
+// sectors and countries by approximated value traded
+db.stocks.aggregate(aggregation);
+
