ab35fcad9a288b4bc88b5afbc9a8372da284d5cd
Emmanbm <93186313+Emmanbm@users.noreply.github.com>
2022-03-26 02:45:50 +0100

    Create simple_queries.js
    I just put queries for the moment, I wanted to create some relationships between stocks and desc to make some queries with graphs but I thought that we had to agree on the names of the relations, so I make queries without a graph in the meantime...

---
+++ neo4j/report/simple_queries.js
@@ -0,0 +1,33 @@
+//Simple Queries
+
+//--1-- Find the number of all companies that contain 'Inc.' in their name.
+
+MATCH (a:Stocks)
+WHERE a.company CONTAINS "Inc."
+RETURN COUNT(a.company) AS number
+
+//-- 2 -- Find the country, the sector and the industry of Tesla.
+
+MATCH (a:Stocks{company:"Tesla Motors, Inc."}) MATCH (b:Description)
+WHERE toInteger(a.desc_id) = b.id
+RETURN a.company AS company, b.country AS country, b.sector AS sector, b.industry AS industry
+
+//-- 3 -- Find the names of all companies in the Technology Sector that are in the USA.
+
+MATCH (a:Stocks) MATCH (b:Description{country:"USA"})
+WHERE toInteger(a.desc_id) = b.id AND b.sector CONTAINS  "Technology"
+RETURN a.company, b.country, b.sector LIMIT 10
+
+//-- 4 -- Find the names, sectors, industries and countries of all companies whose name begins with "Alc" (there are only 3).
+
+MATCH (a:Description) MATCH (b:Stocks)
+WHERE toInteger(b.desc_id) = a.id AND b.company STARTS WITH 'Alc'
+RETURN b.company AS name, a.country AS country, a.sector AS sector, a.industry AS industry
+
+//-- 5 -- Find the names of all companies whose stock price is greater than 1000 (there are only 4).
+
+MATCH (a:Stocks)
+WHERE toFloat(a.price) > 1000
+RETURN a.company, a.price
+
+//-- 6 to be continued...
